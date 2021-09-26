b9d5b9e88e6bc6d1c477112ddc7581e54d728227
Vimesh Shah <vimesh93@gmail.com>
2021-09-26 22:07:47 +0530

    Form created

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,27 +1,88 @@
+import { Formik } from "formik";
 import React from "react";
 import { Button, Form } from "react-bootstrap";
 
 function LoginForm() {
+  const initialValues = { email: "", password: "", remember: false };
+
+  const validateForm = (values) => {
+    const errors = {};
+    if (!values.email) {
+      errors.email = "Please enter email";
+    }
+
+    if (!values.password) {
+      errors.password = "Please enter password";
+    }
+
+    return errors;
+  };
+
+  const onSubmitForm = (values, { setSubmitting }) => {
+    console.log(values);
+    setSubmitting(false);
+  };
+
   return (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Remember Me" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Login
-      </Button>
-    </Form>
+    <Formik
+      initialValues={initialValues}
+      validate={validateForm}
+      onSubmit={onSubmitForm}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            {errors.email && touched.email && (
+              <Form.Text className="text-danger">{errors.email}</Form.Text>
+            )}
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              name="password"
+              placeholder="Enter password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.password}
+            />
+            {errors.password && touched.password && (
+              <Form.Text className="text-danger">{errors.password}</Form.Text>
+            )}
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check
+              type="checkbox"
+              label="Remember Me"
+              name="remember"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.remember}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" disabled={isSubmitting}>
+            Login
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 }
 
---
+++ src/data/fakeProducts.js
@@ -0,0 +1,94 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 13",
+    company: "Apple",
+    price: "999",
+  },
+  {
+    id: 2,
+    name: "iPhone 13 Pro",
+    company: "Apple",
+    price: "1099",
+  },
+  {
+    id: 3,
+    name: "iPhone 13 Pro Max",
+    company: "Apple",
+    price: "1299",
+  },
+  {
+    id: 4,
+    name: "iPad Pro",
+    company: "Apple",
+    price: "1099",
+  },
+  {
+    id: 5,
+    name: "iPad Air",
+    company: "Apple",
+    price: "899",
+  },
+  {
+    id: 6,
+    name: "iMac",
+    company: "Apple",
+    price: "1299",
+  },
+  {
+    id: 7,
+    name: "Macbook Air M1",
+    company: "Apple",
+    price: "999",
+  },
+  {
+    id: 8,
+    name: "Macbook Pro M1",
+    company: "Apple",
+    price: "1299",
+  },
+  {
+    id: 9,
+    name: "Pixel 6",
+    company: "Google",
+    price: "899",
+  },
+  {
+    id: 10,
+    name: "Chrome Book",
+    company: "Google",
+    price: "999",
+  },
+  {
+    id: 11,
+    name: "Surface Pro Laptop",
+    company: "Microsoft",
+    price: "1399",
+  },
+  {
+    id: 12,
+    name: "Surface Go",
+    company: "Microsoft",
+    price: "799",
+  },
+  {
+    id: 13,
+    name: "Surface Duo",
+    company: "Microsoft",
+    price: "1599",
+  },
+  {
+    id: 14,
+    name: "Galaxy Book",
+    company: "Samsung",
+    price: "999",
+  },
+  {
+    id: 15,
+    name: "Galaxy Book Pro",
+    company: "Samsung",
+    price: "1299",
+  },
+];
+
+export default products;
